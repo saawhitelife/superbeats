a374c8d137c736d8ddc84b22e0d7b96dfc1cf9f4
Alexander Kremenchuk <saawhitelife@gmail.com>
2020-04-08 08:50:47 +0300

    Use unittest. Redesign functional tests flow accordingly

--- superbeats/functional_tests.py
+++ superbeats/functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http:/localhost:8000')
 
-assert 'Django' in browser.title
+class NewBeatTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_of_beats_and_access_it_via_url(self):
+        # You visit superbeats
+        self.browser.get('http:/localhost:8000')
+
+        # You see heading and title
+        self.assertIn('Superbeats', self.browser.title)
+        self.fail()
+
+        # superbeats offers you to enter your first beat title
+        # You enter beat title and press enter. Page refreshes
+        # and you see thhe beat title on the page
+
+        # superbeats still offer to enter another beat name
+        # You enter second beat name and press enter
+
+        # Page refreshes and both beats are now in the list
+
+        # You see generated link for the list. The site
+        # stored it for you
+
+        # You visit the link and see the list there
+
+        # Quit browser
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
