52d2e38d7c1285f276d9797cbe93272cd45af9f8
Alexander Kremenchuk <saawhitelife@gmail.com>
2020-04-11 08:34:17 +0300

    Refactor home page test. Added template

--- beats/tests.py
+++ beats/tests.py
@@ -6,13 +6,6 @@ from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        home = resolve('/')
-        self.assertEqual(home.func, home_page)
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(HttpRequest)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Superbeats</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
