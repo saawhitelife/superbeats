8b36ce3de428f163b6218b43180ee67b169416a3
Alexander Kremenchuk <saawhitelife@gmail.com>
2020-04-12 12:46:06 +0300

    Refactored FT

--- superbeats/functional_tests.py
+++ superbeats/functional_tests.py
@@ -11,6 +11,11 @@ class NewBeatTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_text_in_table_rows(self, text):
+        table = self.browser.find_element_by_id('id_beats_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(text, [row.text for row in rows])
+
     def test_can_start_a_list_of_beats_and_access_it_via_url(self):
         # You visit superbeats
         self.browser.get('http:/localhost:8000')
@@ -30,10 +35,7 @@ class NewBeatTest(unittest.TestCase):
         input_box.send_keys('Saawhitelife - Sin City Soul')
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_beats_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Saawhitelife - Sin City Soul', [row.text for row in rows])
+        self.check_for_text_in_table_rows('1: Saawhitelife - Sin City Soul')
 
         # superbeats still offer to enter another beat name
         # You enter second beat name and press enter
@@ -42,10 +44,8 @@ class NewBeatTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
         # Page refreshes and both beats are now in the list
-        table = self.browser.find_element_by_id('id_beats_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Saawhitelife - Sin City Soul', [row.text for row in rows])
-        self.assertIn('2: Grimoire - Sin City Soul', [row.text for row in rows])
+        self.check_for_text_in_table_rows('1: Saawhitelife - Sin City Soul')
+        self.check_for_text_in_table_rows('2: Saawhitelife - Grimoire')
         self.fail('Test end')
         # You see generated link for the list. The site
         # stored it for you
