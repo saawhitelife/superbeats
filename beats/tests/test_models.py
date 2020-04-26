a1cf4c93af2abda9aaefccb9ba5320b5ff881433
saawhitelife <saawhitelife@gmail.com>
2020-04-26 15:13:18 +0300

    module tests divided into 2 files

---
+++ beats/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from beats.models import Beat, BeatList
+
+
+class BeatAndBeatListModelsTest(TestCase):
+    def test_can_save_and_retrieve_beats(self):
+        beat_list = BeatList()
+        beat_list.save()
+
+        first_beat = Beat()
+        first_beat.title = 'Saawhitelife - Sin City Soul'
+        first_beat.beat_list = beat_list
+        first_beat.save()
+
+        second_beat = Beat()
+        second_beat.title = 'Saawhitelife - Grimoire'
+        second_beat.beat_list = beat_list
+
+        second_beat.save()
+
+        saved_beat_list = BeatList.objects.first()
+        self.assertEqual(beat_list, saved_beat_list)
+
+        beats = Beat.objects.all()
+
+        self.assertEqual(beats.count(), 2)
+
+        self.assertEqual(beats[0].title, 'Saawhitelife - Sin City Soul')
+        self.assertEqual(beats[0].beat_list, beat_list)
+        self.assertEqual(beats[1].title, 'Saawhitelife - Grimoire')
+        self.assertEqual(beats[1].beat_list, beat_list)
--- beats/tests/test_views.py
+++ beats/tests/test_views.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from beats.models import Beat, BeatList
-from django.urls import resolve
-from beats.views import home_page
-from django.http import HttpRequest
+
 # Create your tests here.
 
 
@@ -18,33 +16,6 @@ class HomePageTest(TestCase):
         new_beat = Beat.objects.first()
         self.assertEqual(new_beat.title, 'Saawhitelife - Sin City Soul')
 
-class BeatAndBeatListModelsTest(TestCase):
-    def test_can_save_and_retrieve_beats(self):
-        beat_list = BeatList()
-        beat_list.save()
-
-        first_beat = Beat()
-        first_beat.title = 'Saawhitelife - Sin City Soul'
-        first_beat.beat_list = beat_list
-        first_beat.save()
-
-        second_beat = Beat()
-        second_beat.title = 'Saawhitelife - Grimoire'
-        second_beat.beat_list = beat_list
-
-        second_beat.save()
-
-        saved_beat_list = BeatList.objects.first()
-        self.assertEqual(beat_list, saved_beat_list)
-
-        beats = Beat.objects.all()
-
-        self.assertEqual(beats.count(), 2)
-
-        self.assertEqual(beats[0].title, 'Saawhitelife - Sin City Soul')
-        self.assertEqual(beats[0].beat_list, beat_list)
-        self.assertEqual(beats[1].title, 'Saawhitelife - Grimoire')
-        self.assertEqual(beats[1].beat_list, beat_list)
 
 class BeatsViewTest(TestCase):
     def test_display_beats_for_correct_list(self):
