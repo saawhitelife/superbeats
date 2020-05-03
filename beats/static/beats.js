713513d08f0755b724a81b81bab7cedf9c4c6a41
saawhitelife <saawhitelife@gmail.com>
2020-05-03 11:35:26 +0300

    namespace initialize javascript

--- beats/static/beats.js
+++ beats/static/beats.js
@@ -1,4 +1,5 @@
-var initialize = function () {
+window.Superbeats = {}
+window.Superbeats.initialize = function () {
     $('input[name="title"]').on('keypress', function () {
         $('.has-error').hide()
     })
