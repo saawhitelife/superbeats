f0e2ea2bb264cc830f0f8148dd456d2a8b3896b2
saawhitelife <saawhitelife@gmail.com>
2020-05-12 15:13:38 +0300

    use real mail...

--- superbeats/settings.py
+++ superbeats/settings.py
@@ -144,6 +144,10 @@ LOGGING = {
     'root': {'level': 'INFO'}
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SESSION_COOKIE_NAME = 'superbeats-cookie'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'saa1white1life@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
